b2f024114377c00256abf669405e4b755209d0c1
kawarashotaro <kkawaharasshoutarou@gmail.com>
2017-07-11 07:34:55 +0900

    practise

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,14 @@
 #include <jni.h>
 #include <string>
-
+#include <opencv2/core.hpp>
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_example_kawarashotaro_opencvpractise_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    cv::Mat mat;
+    std::ostringstream oss;
+    oss << "rows: " << mat.rows << ", cols: " << mat.cols;
+    std::string str = oss.str();
+    return env->NewStringUTF(str.c_str());
 }
